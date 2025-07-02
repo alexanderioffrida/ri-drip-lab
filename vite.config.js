bc99c9397102bef317a3cfa7d4046eae6246424b
Alexander Flores <alexanderflores@Alexanders-MacBook-Pro.local>
2025-07-02 16:11:57 -0400

    Deploy latest site

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/ri-drip-lab/',
-  plugins: [react(), tailwindcss()],
-})
